5a4b19e4191cb47d191b8b09abeb2c3993f4f22b
Balaji Thukuntala <balajireddy567567.br@gmail.com>
2024-08-18 17:05:06 +0530

    Initial commit with Firebase Hosting workflow

--- lib/main.dart
+++ lib/main.dart
@@ -38,53 +38,61 @@ class MyHomePage extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
+          SizedBox(
+            height: 30,
+          ),
           Image.network(
-              width: 250,
-              height: 250,
-              'https://static.vecteezy.com/system/resources/previews/029/332/148/large_2x/ai-generative-dj-playing-and-mixing-music-in-nightclub-party-at-night-edm-dance-music-club-with-crowd-of-young-people-free-photo.jpg'), // Replace with your image URL
-          const SizedBox(height: 20),
-          ColorDropdownWidget(),
-          SliderWidget(),
+              width: 150,
+              height: 200,
+              'https://static.vecteezy.com/system/resources/previews/029/332/148/large_2x/ai-generative-dj-playing-and-mixing-music-in-nightclub-party-at-night-edm-dance-music-club-with-crowd-of-young-people-free-photo.jpg'),
+          Container(width: 350, child: ColorDropdownWidget()),
+          Container(width: 350, child: SliderWidget()),
           const SizedBox(height: 10),
-          Obx(() => TextField(
-                controller: controller.totalItemsController,
-                decoration: InputDecoration(
-                  labelText: 'Total Items',
-                  labelStyle: TextStyle(color: controller.getSelectedColor()),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide:
-                        BorderSide(color: controller.getSelectedColor()),
+          Container(
+            width: 350,
+            child: Obx(() => TextField(
+                  controller: controller.totalItemsController,
+                  decoration: InputDecoration(
+                    labelText: 'Total Items',
+                    labelStyle: TextStyle(color: controller.getSelectedColor()),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide:
+                          BorderSide(color: controller.getSelectedColor()),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide:
+                          BorderSide(color: controller.getSelectedColor()),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide:
-                        BorderSide(color: controller.getSelectedColor()),
-                  ),
-                ),
-                keyboardType: TextInputType.number,
-                onChanged: (value) => controller.updateProgressIndicators(),
-              )),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) => controller.updateProgressIndicators(),
+                )),
+          ),
           const SizedBox(height: 12),
-          Obx(() => TextField(
-                controller: controller.itemsInLineController,
-                decoration: InputDecoration(
-                  labelText: 'Items in Line',
-                  labelStyle: TextStyle(color: controller.getSelectedColor()),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide:
-                        BorderSide(color: controller.getSelectedColor()),
+          Container(
+            width: 350,
+            child: Obx(() => TextField(
+                  controller: controller.itemsInLineController,
+                  decoration: InputDecoration(
+                    labelText: 'Items in Line',
+                    labelStyle: TextStyle(color: controller.getSelectedColor()),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide:
+                          BorderSide(color: controller.getSelectedColor()),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide:
+                          BorderSide(color: controller.getSelectedColor()),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide:
-                        BorderSide(color: controller.getSelectedColor()),
-                  ),
-                ),
-                keyboardType: TextInputType.number,
-                onChanged: (value) => controller.updateProgressIndicators(),
-              )),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) => controller.updateProgressIndicators(),
+                )),
+          ),
           const SizedBox(
             height: 20,
           ),
